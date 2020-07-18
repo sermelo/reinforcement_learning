2609c2c1b7d1b956217dc9e965721c7f7baa91af
Sergio Merino Alonso <sergio.merino.a@gmail.com>
2020-07-18 18:13:05 +0100

    Add functionality to memory to get date with a cost balance
    The new method allow to balance the batch with instance with and
    without cost

--- lib/memory.py
+++ lib/memory.py
@@ -6,9 +6,15 @@ class Memory(object):
     def __init__(self, size):
         self.size = size
         self.buffer = deque(maxlen=size)
+        self.no_cost_buffer = deque(maxlen=size)
+        self.cost_buffer = deque(maxlen=size)
 
     def push(self, state, action, reward, next_state, cost, fail):
         self.buffer.append((state, action, [reward], next_state, [cost], [fail]))
+        if cost == 0:
+            self.no_cost_buffer.append((state, action, [reward], next_state, [cost], [fail]))
+        else:
+            self.cost_buffer.append((state, action, [reward], next_state, [cost], [fail]))
 
     def get_batch(self, batch_size):
         states = []
@@ -28,6 +34,40 @@ class Memory(object):
             next_states.append(next_state)
             costs.append(cost)
             fails.append(fail)
+        return torch.FloatTensor(states), torch.FloatTensor(actions), torch.FloatTensor(rewards), torch.FloatTensor(next_states), torch.FloatTensor(costs), torch.BoolTensor(fails)
+
+    def get_cost_training_batch(self, batch_size, proportion=0.5):
+        states = []
+        actions = []
+        rewards = []
+        next_states = []
+        costs = []
+        fails = []
+
+        no_cost_batch_size = int(batch_size * proportion)
+        cost_batch_size = batch_size - no_cost_batch_size
+        if (len(self.no_cost_buffer) >= no_cost_batch_size and len(self.cost_buffer) >= cost_batch_size):
+
+            batch = random.sample(self.no_cost_buffer, no_cost_batch_size)
+            for item in batch:
+                state, action, reward, next_state, cost, fail = item
+                states.append(state)
+                actions.append(action)
+                rewards.append(reward)
+                next_states.append(next_state)
+                costs.append(cost)
+                fails.append(fail)
+
+            batch = random.sample(self.cost_buffer, cost_batch_size)
+            for item in batch:
+                state, action, reward, next_state, cost, fail = item
+                states.append(state)
+                actions.append(action)
+                rewards.append(reward)
+                next_states.append(next_state)
+                costs.append(cost)
+                fails.append(fail)
+
         return torch.FloatTensor(states), torch.FloatTensor(actions), torch.FloatTensor(rewards), torch.FloatTensor(next_states), torch.FloatTensor(costs), torch.BoolTensor(fails)
 
     def __len__(self):
